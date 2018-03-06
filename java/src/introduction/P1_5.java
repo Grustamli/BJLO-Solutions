5ab5cc3c2d632178be85806c03e760a576f911ec
Gadir Rustamli <qedir_rustemli@hotmail.com>
2018-03-07 02:12:41 +0400

    questions added

---
+++ java/src/introduction/P1_5.java
@@ -0,0 +1,15 @@
+/*
+
+• P 1.5 Write a program that displays your name inside a box on the screen
+
+ */
+
+package introduction;
+
+public class P1_5 {
+    public static void main(String[] args) {
+        System.out.println("+---------+");
+        System.out.println("|  GADIR  |");
+        System.out.println("+---------+");
+    }
+}
---
+++ java/src/introduction/P1_6.java
@@ -0,0 +1,22 @@
+/*
+
+••• P 1.6 Write a program that prints your name in large letters, such as
+*   *    **    ****   ****   *   *
+*   *  *    *  *   *  *   *  *   *
+*****  *    *  ****   ****    * *
+*   *  ******  *   *  *   *    *
+*   *  *    *  *   *  *   *    *
+
+ */
+
+package introduction;
+
+public class P1_6 {
+    public static void main(String[] args) {
+        System.out.println("*   *    **    ****   ****   *   *");
+        System.out.println("*   *  *    *  *   *  *   *  *   *");
+        System.out.println("*****  *    *  ****   ****    * *");
+        System.out.println("*   *  ******  *   *  *   *    *");
+        System.out.println("*   *  *    *  *   *  *   *    *");
+    }
+}
